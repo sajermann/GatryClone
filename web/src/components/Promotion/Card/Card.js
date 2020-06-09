77635fa00bc2290726069f70703683055a2f5fcf
Bruno Sajermann <contato@sajermann.com>
2020-06-08 23:11:05 -0300

    Video 8 Complete

--- web/src/components/Promotion/Card/Card.js
+++ web/src/components/Promotion/Card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.css'
 
 const PromotionCard = ({promotion}) => (
@@ -14,6 +15,7 @@ const PromotionCard = ({promotion}) => (
 
         <div className="promotion-card__comments-count" >{promotion.comments.length} {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}</div>
         <a href={promotion.url} target="_blank" rel="noopener noreferrer" className="promotion-card__link" >Ir para o site</a>
+        <Link to={`/edit/${promotion.id}`}>Editar</Link>
       </footer>
     </div>  
   </div>
--- web/src/components/Promotion/Form/Form.js
+++ web/src/components/Promotion/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import './Form.css';
@@ -11,9 +11,19 @@ const initialValue = {
   price: 0
 }
 
-function PromotionForm() {
-  const [values, setValues] = useState(initialValue);
+function PromotionForm({ id }) {
+  const [values, setValues] = useState(id ? null : initialValue);
   const history = useHistory();
+  
+
+  useEffect(()=>{
+    if(id){
+      axios.get(`http://localhost:5000/promotions/${id}`)
+        .then((response) =>{
+          setValues(response.data)
+        })
+    }
+  }, [])
 
   function onChange(ev){
     const { name, value } = ev.target;
@@ -26,7 +36,12 @@ function PromotionForm() {
   function onSubmit(ev) {
     ev.preventDefault();
 
-    axios.post('http://localhost:5000/promotions', values)
+    const method = id ? 'put' : 'post';
+    const url = id
+    ? `http://localhost:5000/promotions/${id}`
+    : 'http://localhost:5000/promotions'
+
+    axios[method](url, values)
       .then((response) => {
         history.push('/')
       });
@@ -36,27 +51,35 @@ function PromotionForm() {
     <div>
       <h1>Promo Show</h1>
       <h2>Nova Promoção</h2>
-      <form onSubmit={onSubmit}>
-        <div className="promotion-form__group">
-          <label htmlFor="title">Título</label>
-          <input id="title" name="title" type="text" onChange={onChange} />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="url">Link</label>
-          <input id="url" name="url" type="text" onChange={onChange} />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="imageUrl">Imagem (URL)</label>
-          <input id="imageUrl" name="imageUrl" type="text" onChange={onChange} />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="price">Preço</label>
-          <input id="price" name="price" type="number" onChange={onChange} />
-        </div>
-        <div>
-          <button type="submit">Salvar</button>
-        </div>
-      </form>
+      {!values 
+        ? (
+          <div>Carregando...</div>
+        )
+        : (
+          <form onSubmit={onSubmit}>
+            <div className="promotion-form__group">
+              <label htmlFor="title">Título</label>
+              <input id="title" name="title" type="text" onChange={onChange} value={values.title} />
+            </div>
+            <div className="promotion-form__group">
+              <label htmlFor="url">Link</label>
+              <input id="url" name="url" type="text" onChange={onChange} value={values.url} />
+            </div>
+            <div className="promotion-form__group">
+              <label htmlFor="imageUrl">Imagem (URL)</label>
+              <input id="imageUrl" name="imageUrl" type="text" onChange={onChange} value={values.imageUrl} />
+            </div>
+            <div className="promotion-form__group">
+              <label htmlFor="price">Preço</label>
+              <input id="price" name="price" type="number" onChange={onChange} value={values.price} />
+            </div>
+            <div>
+              <button type="submit">Salvar</button>
+            </div>
+          </form>
+        )
+      }
+      
     </div>
   );
 }

a73941de52a982334bf2eea693d7cc506af3f749
Bruno Sajermann <contato@sajermann.com>
2020-06-08 22:28:49 -0300

    Video 7 Complete

--- web/src/pages/Promotion/Form/Form.js
+++ web/src/pages/Promotion/Form/Form.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
+import PromotionForm from 'components/Promotion/Form/Form';
+import UIContainer from 'components/UI/Container/Container';
+
 
 const PagesPromotionForm = () => {
   const { id } = useParams();
   return (
-    <div>
-      Form
-      { id && <div>id: {id}</div>}
-    </div>
+    <UIContainer>
+      <PromotionForm />
+    </UIContainer>
   )
 }
 
--- web/src/pages/Promotion/Search/Search.js
+++ web/src/pages/Promotion/Search/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PromotionSearch from 'components/Promotion/Search/Search';
-import UIContainer from 'components/UI/Container/Container'
+import UIContainer from 'components/UI/Container/Container';
 
 const PagesPromotionSearch = () => {
 
